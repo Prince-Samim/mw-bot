1c223cbe3675acd7f412bc54ec4203c5957fd89a
Dark Prince <sksamim225225@gmail.com>
2026-03-24 11:02:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # --- CONFIGURATION ---
 BOT_TOKEN = os.environ.get("8669291841:AAGJzGF5ZVb95ypH8_GWIFWOGnOt4DbDY84")
 CHAT_ID = os.environ.get("@MWjobalart")
-SESSION_ID = os.environ.get("kmq6pig62tpmmjd5gbsh4gsbru")
+SESSION_ID = os.environ.get("va398qh99aof6n0v3i89cc9ulh")
 
 scraper = cloudscraper.create_scraper()
 old_jobs = set()
